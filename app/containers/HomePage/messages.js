4b58ddc725f91ce583ed0af0e8d69cadda82d630
Abijith K <abijith973@gmail.com>
2018-05-02 02:17:44 +0530

    adding change

--- app/containers/HomePage/messages.js
+++ app/containers/HomePage/messages.js
@@ -8,7 +8,7 @@ import { defineMessages } from 'react-intl';
 export default defineMessages({
   startProjectHeader: {
     id: 'boilerplate.containers.HomePage.start_project.header',
-    defaultMessage: 'Start your next react project in seconds',
+    defaultMessage: 'Hello World',
   },
   startProjectMessage: {
     id: 'boilerplate.containers.HomePage.start_project.message',
